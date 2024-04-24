f6550472594ab090dc8f23b3109b76e1fc747c96
Erokhin Sergei <erokhinsergeiinv@gmail.com>
2024-04-24 21:28:05 +0300

    add widget page

--- app/views/parts/footer.php
+++ app/views/parts/footer.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\widgets\page\Page;
 use wfm\View;
 
 /**  @var $this View */
@@ -7,75 +8,76 @@ use wfm\View;
 
 
 <footer>
-    <section class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-6">
-                    <h4> <?php __('tpl_information') ?>  </h4>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Главная</a></li>
-                        <li><a href="#">О магазине</a></li>
-                        <li><a href="#">Оплата и доставка</a></li>
-                        <li><a href="#">Контакты</a></li>
-                    </ul>
-                </div>
+  <section class="footer">
+    <div class="container">
+      <div class="row">
 
-                <div class="col-md-3 col-6">
-                    <h4><?php __('tpl_work_hours') ?> </h4>
-                    <ul class="list-unstyled">
-                        <li>г. Калуга, ул. Пушкина, 10</li>
-                        <li>пн-вс: 9:00 - 18:00</li>
-                        <li>без перерыва</li>
-                    </ul>
-                </div>
-
-                <div class="col-md-3 col-6">
-                    <h4><?php __('tpl_contacts') ?></h4>
-                    <ul class="list-unstyled">
-                        <li><a href="tel:5551234567">555 123-45-67</a></li>
-                        <li><a href="tel:5551234567">555 123-45-68</a></li>
-                        <li><a href="tel:5551234567">555 123-45-69</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-3 col-6">
-                    <h4><?php __('tpl_we_online') ?></h4>
-                    <div class="footer-icons">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-youtube"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
+        <div class="col-md-3 col-6">
+          <h4> <?php __('tpl_information') ?>  </h4>
+          <?php new Page([
+            'cache' => 0,
+            'class' => 'list-unstyled',
+            'prepend' => '<li><a href ="' . base_url() . '">' . ___('tpl_home_link') . '</a></li>',
+          ])
+          ?>
         </div>
-    </section>
+
+        <div class="col-md-3 col-6">
+          <h4><?php __('tpl_work_hours') ?> </h4>
+          <ul class="list-unstyled">
+            <li>г. Калуга, ул. Пушкина, 10</li>
+            <li>пн-вс: 9:00 - 18:00</li>
+            <li>без перерыва</li>
+          </ul>
+        </div>
+
+        <div class="col-md-3 col-6">
+          <h4><?php __('tpl_contacts') ?></h4>
+          <ul class="list-unstyled">
+            <li><a href="tel:5551234567">555 123-45-67</a></li>
+            <li><a href="tel:5551234567">555 123-45-68</a></li>
+            <li><a href="tel:5551234567">555 123-45-69</a></li>
+          </ul>
+        </div>
+
+        <div class="col-md-3 col-6">
+          <h4><?php __('tpl_we_online') ?></h4>
+          <div class="footer-icons">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </footer>
 
 <button id="top">
-    <i class="fas fa-angle-double-up"></i>
+  <i class="fas fa-angle-double-up"></i>
 </button>
 
 <div class="modal fade" id="cart-modal" tabindex="-1" aria-labelledby="exampleModalLabel"
      aria-hidden="true">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">
-                  <?php __('tpl_cart_title') ?>
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
-            </div>
-            <div class="modal-cart-content"></div>
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">
+          <?php __('tpl_cart_title') ?>
+        </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                aria-label="Close"></button>
+      </div>
+      <div class="modal-cart-content"></div>
 
-        </div>
     </div>
+  </div>
 </div>
 
 <?php $this->getDbLogs(); ?>
 
 <script>
-    const PATH = "<?= PATH?>";
+  const PATH = "<?= PATH?>";
 </script>
 <script src="<?= PATH ?>/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"
